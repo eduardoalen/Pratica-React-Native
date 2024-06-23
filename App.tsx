28c1db467e74d4307a898fe0be139e2787efd32d
Eduardo Alencar <alencareduardo17@gmail.com>
2024-06-23 19:36:37 -0300

    Utilizando useState

--- App.tsx
+++ App.tsx
@@ -3,16 +3,19 @@ import { View, Text, Button } from 'react-native';
 
 function App() {
   const [nome, setNome] = useState('Eduardo');
+  const [idade, setIdade] = useState(22);
 
-  function entrar(){
-    setNome('Desodorante');
+  function entrar(nome, idade){
+    setNome(nome);
+    setIdade(idade)
   }
 
   return(
     <View style={{ marginTop: 25 }}>
 
-      <Button title="Mudar nome" onPress={entrar} />
+      <Button title="Mudar nome" onPress={ () => entrar('Junior Alex', 33)} />
       <Text style={{ fontSize: 19 }}> {nome} </Text>
+      <Text style={{ fontSize: 19 }}> {idade} </Text>
 
   </View>
   );
