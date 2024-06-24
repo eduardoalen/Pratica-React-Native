9a67d5d4732bdd62f86dddea278ce51038d5b9b7
Eduardo Alencar <alencareduardo17@gmail.com>
2024-06-24 07:28:56 -0300

    Flexbox e alinhamentos

--- App.tsx
+++ App.tsx
@@ -4,30 +4,42 @@ import { View, Text, StyleSheet} from 'react-native';
 function App() {
   
   return(
-  <View style={{flex:1, backgroundColor: '#121212'}}>
+  <View style={{ 
+    flex:1, 
+    flexDirection: 'row', // linha
+    alignItems: 'flex-start', // eixo Y
+    justifyContent:'center' // eixo x 
 
-    <View style={styles.quadrado1}></View>
-    <View style={styles.quadrado2}>
-      <Text>Sou um texto</Text>
-    </View>
-    <View style={styles.quadrado3}></View>
+    // flexDirection: 'column', // coluna
+    // alignItems: 'flex-start', // eixo X
+    // justifyContent:'center' // eixo Y 
+    }}>
+
+    <View style={[styles.quadrado, styles.preto]}></View>
+    <View style={[styles.quadrado, styles.vermelho]}></View>
+    <View style={[styles.quadrado, styles.verde]}></View>
+    <View style={[styles.quadrado, styles.azul]}></View>
 
   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  quadrado1: {
-    height: 65,
+  quadrado: {
+    height: 50,
+    width: 50
+  },
+  preto: {
     backgroundColor: '#121212'
   },
-  quadrado2: {
-    flex: 1,
-    backgroundColor: '#DDD'
+  vermelho: {
+    backgroundColor: 'red'
   },
-  quadrado3: {
-    height: 65,
-    backgroundColor: '#121212'
+  verde: {
+    backgroundColor: 'green'
+  },
+  azul: {
+    backgroundColor: 'blue'
   }
 });
 
--- anotacoes.tsx
+++ anotacoes.tsx
@@ -104,3 +104,55 @@
 // });
 
 // export default App;
+
+
+
+
+
+// FLEXBOX E ALINHAMENTO --------------------------------------------------
+// import React from 'react';
+// import { View, Text, StyleSheet} from 'react-native';
+
+// function App() {
+  
+//   return(
+//   <View style={{ 
+//     flex:1, 
+//     flexDirection: 'row', // linha
+//     alignItems: 'flex-start', // eixo Y
+//     justifyContent:'center' // eixo x 
+
+//     // flexDirection: 'column', // coluna
+//     // alignItems: 'flex-start', // eixo X
+//     // justifyContent:'center' // eixo Y 
+//     }}>
+
+//     <View style={[styles.quadrado, styles.preto]}></View>
+//     <View style={[styles.quadrado, styles.vermelho]}></View>
+//     <View style={[styles.quadrado, styles.verde]}></View>
+//     <View style={[styles.quadrado, styles.azul]}></View>
+
+//   </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   quadrado: {
+//     height: 50,
+//     width: 50
+//   },
+//   preto: {
+//     backgroundColor: '#121212'
+//   },
+//   vermelho: {
+//     backgroundColor: 'red'
+//   },
+//   verde: {
+//     backgroundColor: 'green'
+//   },
+//   azul: {
+//     backgroundColor: 'blue'
+//   }
+// });
+
+// export default App;
