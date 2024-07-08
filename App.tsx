b03e12a1235cc76b2ccebd380a95f1ff7b63e838
Eduardo Alencar <alencareduardo17@gmail.com>
2024-07-08 07:07:40 -0300

    Utilizando FlatLista

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { View, StyleSheet, ScrollView} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList} from 'react-native';
 
 function App() {
+
+  const [feed, setFeed] = useState([
+    {id: '1', nome: 'Julio', idade: 50, email: 'julio@julio.com'},
+    {id: '2', nome: 'Lucas', idade: 20, email: 'lucas@lucas.com'},
+    {id: '3', nome: 'Henrique', idade: 40, email: 'henrique@henrique.com'},
+    {id: '4', nome: 'Ana', idade: 19, email: 'ana@ana.com'},
+    {id: '5', nome: 'Beatriz', idade: 28, email: 'bea@bea.com'}
+  ]);
 
   return(
   <View style={styles.container}>
 
-    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-      {/* Todas a Views que eu quero que seja scrollavel*/}
-      <View style={styles.box1}></View>
-      <View style={styles.box2}></View>
-      <View style={styles.box3}></View>
-      <View style={styles.box4}></View>
-      <View style={styles.box2}></View>
-    </ScrollView>
+    <FlatList
+    showsVerticalScrollIndicator={false}
+    // passa a lista
+    data={feed}
+    // passar e renderizar a lista
+    renderItem={ ({ item }) => <Pessoa data={item} />} 
+    />
 
   </View>
   );
@@ -23,26 +30,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  box1: {
-    width: 150,
-    height: 250,
-    backgroundColor: 'red'
+  areaPessoa: {
+    backgroundColor: '#121212',
+    height: 200,
+    marginBottom: 15,
+    justifyContent: 'center'
   },
-  box2: {
-    width: 150,
-    height: 250,
-    backgroundColor: 'green'
-  },
-  box3: {
-    width: 150,
-    height: 250,
-    backgroundColor: 'yellow'
-  },
-  box4: {
-    width: 150,
-    height: 250,
-    backgroundColor: 'blue'
+  textoPessoa: {
+    color: '#FFF',
+    fontSize: 20
   }
 });
+
+function Pessoa(props) {
+  return(
+    <View style={styles.areaPessoa}>
+      <Text style={styles.textoPessoa} >{props.data.nome}</Text>
+      <Text style={styles.textoPessoa} >{props.data.idade}</Text>
+      <Text style={styles.textoPessoa} >{props.data.email}</Text>
+    </View>
+  );
+}
 
 export default App;
--- anotacoes.tsx
+++ anotacoes.tsx
@@ -321,3 +321,125 @@
 // });
 
 // export default App;
+
+
+
+
+// import React, { useState } from 'react';
+// import { View, Text, StyleSheet, FlatList} from 'react-native';
+
+// function App() {
+
+//   const [feed, setFeed] = useState([
+//     {id: '1', nome: 'Julio', idade: 50, email: 'julio@julio.com'},
+//     {id: '2', nome: 'Lucas', idade: 20, email: 'lucas@lucas.com'},
+//     {id: '3', nome: 'Henrique', idade: 40, email: 'henrique@henrique.com'},
+//     {id: '4', nome: 'Ana', idade: 19, email: 'ana@ana.com'},
+//     {id: '5', nome: 'Beatriz', idade: 28, email: 'bea@bea.com'}
+//   ]);
+
+//   return(
+//   <View style={styles.container}>
+
+//     <FlatList
+//     showsVerticalScrollIndicator={false}
+//     // passa a lista
+//     data={feed}
+//     // passar e renderizar a lista
+//     renderItem={ ({ item }) => <Pessoa data={item} />} 
+//     />
+
+//   </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1
+//   },
+//   areaPessoa: {
+//     backgroundColor: '#121212',
+//     height: 200,
+//     marginBottom: 15,
+//     justifyContent: 'center'
+//   },
+//   textoPessoa: {
+//     color: '#FFF',
+//     fontSize: 20
+//   }
+// });
+
+// function Pessoa(props) {
+//   return(
+//     <View style={styles.areaPessoa}>
+//       <Text style={styles.textoPessoa} >{props.data.nome}</Text>
+//       <Text style={styles.textoPessoa} >{props.data.idade}</Text>
+//       <Text style={styles.textoPessoa} >{props.data.email}</Text>
+//     </View>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { View, Text, StyleSheet, FlatList} from 'react-native';
+
+// function App() {
+
+//   const [feed, setFeed] = useState([
+//     {_id: '1', nome: 'Julio', idade: 50, email: 'julio@julio.com'},
+//     {_id: '2', nome: 'Lucas', idade: 20, email: 'lucas@lucas.com'},
+//     {_id: '3', nome: 'Henrique', idade: 40, email: 'henrique@henrique.com'},
+//     {_id: '4', nome: 'Ana', idade: 19, email: 'ana@ana.com'},
+//     {_id: '5', nome: 'Beatriz', idade: 28, email: 'bea@bea.com'}
+//   ]);
+
+//   return(
+//   <View style={styles.container}>
+
+//     <FlatList
+//     showsVerticalScrollIndicator={false}
+//     // avisa que o _id é o id
+//     keyExtractor={ (item) => item._id }
+//     // passa a lista
+//     data={feed}
+//     // passar e renderizar a lista
+//     renderItem={ ({ item }) => <Pessoa data={item} />} 
+//     />
+
+//   </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1
+//   },
+//   areaPessoa: {
+//     backgroundColor: '#121212',
+//     height: 200,
+//     marginBottom: 15,
+//     justifyContent: 'center'
+//   },
+//   textoPessoa: {
+//     color: '#FFF',
+//     fontSize: 20
+//   }
+// });
+
+// function Pessoa(props) {
+//   return(
+//     <View style={styles.areaPessoa}>
+//       <Text style={styles.textoPessoa} >{props.data.nome}</Text>
+//       <Text style={styles.textoPessoa} >{props.data.idade}</Text>
+//       <Text style={styles.textoPessoa} >{props.data.email}</Text>
+//     </View>
+//   );
+// }
+
+// export default App;
